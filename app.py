12cef1f3fa9e744922c6d057956cf37ca5aef8ef
Stephanie Kasaon <stephaniekasaon@gmail.com>
2022-03-04 21:18:36 +0900

    first commit

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def home():
 
-    return render_template("index.html")
+    return render_template("index2.html")
 
 
 def gen(camera):
